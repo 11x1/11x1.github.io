29228311ed63b4c001c00885e2c83199d7b29543
11x1 <89593923+11x1@users.noreply.github.com>
2022-11-27 00:06:26 +0200

    Update web_handleing.js

--- eclipse/web_handleing.js
+++ eclipse/web_handleing.js
@@ -3,7 +3,7 @@ const header_logo = document.getElementsByClassName('pill')[0]
 const arrows_div = document.getElementsByClassName('arrows-down')[0]
 const footer = document.getElementsByClassName('footer')[0]
 
-header_logo.addEventListener('mousedown', () => window.location.href = 'https://discord.gg/desync')
+header_logo.addEventListener('mousedown', () => window.location.href = 'https://discord.gg/TcCUBpmpCp')
 arrows_div.addEventListener('mousedown', () => footer.scrollIntoView({behavior: "smooth"}))
 const update_body_padding = () => {
     document.body.style.paddingTop = header.offsetHeight + header.offsetHeight/10 + 'px'
